dd36ddfb5419f9753f4d27904e0419b9bc234e28
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-04-17 12:24:52 +0530

    Form Handling done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import useForm from "./hooks/useForm";
+
 const App = () => {
-  return <div>App</div>;
+  const { values, handleChange, handleSubmit } = useForm({
+    username: "",
+    email: "",
+  });
+
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl mb-4">Custom Hooks in Action</h1>
+      <div className="mb-4">
+        <h2 className="text-xl">Form Handling Example</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="border p-2 mb-2"
+            name="username"
+            value={values.username}
+            onChange={handleChange}
+            placeholder="Username"
+          />
+          <input
+            type="email"
+            className="border p-2 mb-2"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            placeholder="Email"
+          />
+          <button type="submit" className="border p-2">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default App;
